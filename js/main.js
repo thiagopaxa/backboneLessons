23215ffca57e3cdc3f6607196806ea9095e17b7f
Thiago dos Santos Paixão <tpaxadev@gmail.com>
2015-01-04 18:24:29 -0200

    Lesson 9

--- js/main.js
+++ js/main.js
@@ -1,57 +1,61 @@
+// Person Model
 var Person = Backbone.Model.extend({
   
   defaults: {
     name: 'John Doe',
     age: 33,
-    occupation: 'worker',
+    occupation: 'worker'
   },
   
   validate : function(attrs){
-    // the validate method is called whenever a property is set.
-    // attrs is just like Person.toJson()
-
+  
     if ( Number(attrs.age) < 0 ){
       return "The age must be positive, jackass.";
     }
     if ( ! attrs.name ){
       return "Every Person should have a name";
     }
-
   },
 
   work : function(){
     return this.get('name') + ' is now working';
   }
 })
-
-var Thiago = new Person({age:24,name:'Thiagão',occupation:'Developer'})
-
-Person.prototype.on('invalid',function(model,error){
-  // on invalid is the listener that will return those messages within the validate method
-  console.error(error)
-
-});
-
+// The collection of persons
+var PeopleCollection = Backbone.Collection.extend({
+  model:Person
+})
+// View For a Person
 var PersonView = Backbone.View.extend({
-  // at the view you can define 
-  // classes, ids and tag for the element of the view.
+  
   tagName: 'li',
   className: 'person',
-  //For the template here we used the template engine of UNDERSCORE
   template : _.template( $('#personTemplate').html() ),
   
   initialize : function() {
-    // initialize is the constructor method for backbone views
     this.render();
   },
   render: function(){
-
     this.$el.html( this.template( this.model.toJSON() ) );
-  
   }
 
 });
 
-var thiagoView = new PersonView({model: Thiago});
-// now you can call the view for this with
-// thiagoView.el or thiagoView.$el (for jquery methods)
+// Collection instantiated
+var people = new PeopleCollection([
+  {
+    name: 'Jorge',
+    age: 20,
+    occupation: 'pedreiro'
+  },
+  {
+    name: 'Carlos',
+    age: 22,
+    occupation: 'farmaceutico'
+  },
+  {
+    name: 'Thiago',
+    age: 24,
+    occupation: 'Dev'
+  },
+])
