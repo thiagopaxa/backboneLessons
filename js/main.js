59df22665a53ddd46f3fc2a967c9c477107e5ecd
Thiago dos Santos Paixão <tpaxadev@gmail.com>
2015-01-05 22:18:30 -0200

    Lesson 12

--- js/main.js
+++ js/main.js
@@ -1,86 +1,100 @@
-var templateMaker = function(id){
-  return _.template($('#' + id).html());
-}
-// Person Model
-var Person = Backbone.Model.extend({
+(function(){
   
-  defaults: {
-    name: 'John Doe',
-    age: 33,
-    occupation: 'worker'
-  },
-  
-  validate : function(attrs){
-  
-    if ( Number(attrs.age) < 0 ){
-      return "The age must be positive, jackass.";
-    }
-    if ( ! attrs.name ){
-      return "Every Person should have a name";
-    }
-  },
+  window.App = {
+    Models: {},
+    Views: {},
+    Collections: {}
 
-  work : function(){
-    return this.get('name') + ' is now working';
-  }
-})
-// The collection of persons
-var PeopleCollection = Backbone.Collection.extend({
-  model:Person
-})
-// View For a Person
-var PersonView = Backbone.View.extend({
-  
-  tagName: 'li',
-  className: 'person',
-  template : templateMaker('personTemplate'),
-  
-  render: function(){
-    this.$el.html( this.template( this.model.toJSON() ) );
-    return this;
-  }
+  };
 
-});
-
-// View For all people
-var PeopleView = Backbone.View.extend({
-  tagName: 'ul',
-  render: function(){
-    // this.collection is passed in the instance
-    // and the .each method is one of the many underscore built in helpers
-    this.collection.each(function(person){
+  window.templateMaker = function(id){
+    return _.template($('#' + id).html());
+  };
+  
+  // Person Model
+  App.Models.Person = Backbone.Model.extend({
     
-      var personView = new PersonView({model:person})
-      this.$el.append(personView.render().el);
+    defaults: {
+      name: 'John Doe',
+      age: 33,
+      occupation: 'worker'
+    },
     
-    },this); 
-    // 'this' is passed like a binding property
-    // to use the collection 'this', instead of the window 'this'
-    return this;
-  }
+    validate : function(attrs){
+    
+      if ( Number(attrs.age) < 0 ){
+        return "The age must be positive, jackass.";
+      }
+      if ( ! attrs.name ){
+        return "Every Person should have a name";
+      }
+    },
 
-});
+    work : function(){
+      return this.get('name') + ' is now working';
+    }
+  })
 
-// Collection instantiated
-var people = new PeopleCollection([
-  {
-    name: 'Jorge',
-    age: 20,
-    occupation: 'pedreiro'
-  },
-  {
-    name: 'Carlos',
-    age: 22,
-    occupation: 'farmaceutico'
-  },
-  {
-    name: 'Thiago',
-    age: 24,
-    occupation: 'Dev'
-  },
-])
+  // The collection of persons
+  App.Collections.People = Backbone.Collection.extend({
+    model: App.Models.Person
+  })
+  // View For a Person
+  App.Views.Person = Backbone.View.extend({
+    
+    tagName: 'li',
+    className: 'person',
+    template : templateMaker('personTemplate'),
+    
+    render: function(){
+      this.$el.html( this.template( this.model.toJSON() ) );
+      return this;
+    }
 
-var peopleView = new PeopleView({
-  collection: people
-});
-$('body').append(peopleView.render().el);
+  });
+
+  // View For all people
+  App.Views.People = Backbone.View.extend({
+    
+    tagName: 'ul',
+    render: function(){
+      // this.collection is passed in the instance
+      // and the .each method is one of the many underscore built in helpers
+      this.collection.each(function(person){
+      
+        var personView = new App.Views.Person({model:person})
+        this.$el.append(personView.render().el);
+      
+      },this); 
+      // 'this' is passed like a binding property
+      // to use the collection 'this', instead of the window 'this'
+      return this;
+    }
+
+  });
+
+  // Collection instantiated
+  var people = new App.Collections.People([
+    {
+      name: 'Jorge',
+      age: 20,
+      occupation: 'pedreiro'
+    },
+    {
+      name: 'Carlos',
+      age: 22,
+      occupation: 'farmaceutico'
+    },
+    {
+      name: 'Thiago',
+      age: 24,
+      occupation: 'Dev'
+    },
+  ])
+
+  var peopleView = new App.Views.People({
+    collection: people
+  });
+  $('body').append(peopleView.render().el);
+
+})();
