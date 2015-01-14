5bd6163d01cd33aa83a8f88af9cbe26cd3547c73
Thiago dos Santos Paixão <tpaxadev@gmail.com>
2015-01-13 23:59:31 -0200

    Lesson 18

--- js/main.js
+++ js/main.js
@@ -11,13 +11,21 @@
   App.Router = Backbone.Router.extend({
     routes:{
       '': 'index',
-      'show': 'show'
+      'show/:id': 'show',
+      'download/*filename': 'download',
+      '*other': 'default'
     },
     index: function(){
       console.log("Hi Router");
     },
-    show: function(){
-      console.log("This is a show");
+    show: function(id){
+      console.log("Show id of "+ id);
+    },
+    default: function(other){
+      console.error('There is nothing here, you requested: ' + other);
+    },
+    download: function(filename){
+      console.log("You want the file: " + filename);
     }
   })
   var router = new App.Router;
